2967383ad05f473f1160afc6b9dcce9ae8844eb6
ITxxxxMChandirasekaran <manjula.chandirasekaran@tri-c.edu>
2020-02-19 08:29:03 -0500

    commit

---
+++ FindTheBugsinCode/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheBugsinCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Let us find the bugs and squash them");          
+
+            /*
+            Console.WriteLine("Exercise1  - Goal is to add 2 numbers and display sum. I get compilation error. Why?");
+            int x = 10;
+            int y = 20;
+            int sum = 0;
+            x + y = sum;
+            Console.WriteLine("Exercise #1: sum="+sum);
+            */
+
+           
+
+            /*
+             Console.WriteLine("Exercise2 - I want to multiply a given number by 2. But I get compilation error. Why?");
+             Console.WriteLine("Enter a number");
+             int input = Console.ReadLine();
+             input * 2;
+             Console.WriteLine("Exercise #2: input * 2 = "+ input);
+             */
+
+           
+
+            /*
+            Console.WriteLine("Exercise3 - I want to check if the input is 5 but i get compilation error. Why?");
+            Console.WriteLine("Enter a color");
+            string color = Console.ReadLine();
+            if (color = "red");
+            {
+                Console.WriteLine("Your favorite color is red and so is mine...");
+            }
+            else
+            {
+                Console.WriteLine("Your favorite color is not red and so you are not cool!");            
+            }
+            */
+
+           
+
+            /*
+             Console.WriteLine("Exercise4  - Goal is to print numbers 0-4 but I get compilation error. Why?");
+             int countr = 10;
+             for (int counter = 0; counter < 5; counter++);
+             {
+                 Console.WriteLine("counter value is "+ countr);
+             }
+           */
+
+         
+
+            /* 
+            Console.WriteLine("Exercise5  - Goal is to print 0 - 9. I get no output. Why?");
+            for (int i = 0; i > 10; i++)
+            {
+                Console.WriteLine("here's the value of i: "+ i);
+            }
+            */
+
+          
+
+            /*
+             Console.WriteLine("Exercise6  - Goal is to display 10 to 0 but i get the same output forever!!!!");
+            int g = 10;
+            int a = 0;
+            while( g >= 0)
+            {
+                a = g - 2;
+                Console.WriteLine("a = " + a);
+            }
+            */
+
+      
+
+            Console.ReadKey();
+        }
+    }
+}
